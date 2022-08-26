543b8d4d60719c42096dc8f35fab55908feb21b6
Nghiem Gia Bao <bao15022016@gmail.com>
2022-08-26 09:09:47 +0700

    Ready to post

---
+++ assets/javascript/main.js
@@ -0,0 +1,32 @@
+async function getData(url){
+    try{
+        const response = await fetch(url);
+        if(response.ok){
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            const adviceNumber = document.getElementById('advice_number');
+            const advice = document.getElementById('advice');
+            adviceNumber.innerHTML = jsonResponse.slip.id;
+            advice.innerHTML = jsonResponse.slip.advice;
+
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+
+
+function init(){
+    const url = "https://api.adviceslip.com/advice";
+    const changeAdvice = document.getElementById("changeAdvice");
+    getData(url);
+    changeAdvice.addEventListener("click", function(){
+        getData(url);
+    });
+    
+
+
+}
+
+window.onload = init;
